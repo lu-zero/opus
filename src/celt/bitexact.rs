751130b6ff2ad47ca4e654d9135fcfce4f663da9
Luca Barbato <lu_zero@gentoo.org>
2019-08-18 20:33:08 +0200

    Fix bitexact functions

--- src/celt/bitexact.rs
+++ src/celt/bitexact.rs
@@ -19,15 +19,16 @@ pub fn frac_mul16(a: i16, b: i16) -> i16 {
 pub fn cos(x: i16) -> i16 {
     let v = ((mul16(x, x) + 4096) >> 13) as i16;
 
-    1 + (32767 - v) + frac_mul16(v, -7651 + frac_mul16(v, 8277 + frac_mul16(-626, x)))
+    1 + (32767 - v) + frac_mul16(v, -7651 + frac_mul16(v, 8277 + frac_mul16(-626, v)))
 }
 
 #[inline(always)]
 pub fn log2tan(isin: i32, icos: i32) -> i32 {
+    let ls = isin.ilog();
     let lc = icos.ilog();
-    let ls = icos.ilog();
+
+    let isin = (isin << (15 - ls)) as i16;
     let icos = (icos << (15 - lc)) as i16;
-    let isin = (isin << (16 - ls)) as i16;
 
     let s = (ls << 11) + frac_mul16(isin, frac_mul16(isin, -2597) + 7932) as i32;
     let c = (lc << 11) + frac_mul16(icos, frac_mul16(icos, -2597) + 7932) as i32;
